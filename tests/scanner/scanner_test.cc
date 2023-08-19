f05d8be3b1c148c98730bed9ca51dc4fc99fb170
Vladimir Mishel <vmishel@outlook.com>
2023-08-18 19:36:41 -0700

    Removed unnecessary logging flag in scanner_test

--- tests/scanner/scanner_test.cc
+++ tests/scanner/scanner_test.cc
@@ -59,7 +59,6 @@ TEST_F(ScannerTest, TestScanIntegerLiterals) {
 }
 
 int main(int argc, char **argv) {
-	FLAGS_logtostdout = true;
 	::google::InitGoogleLogging(argv[0]);
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
