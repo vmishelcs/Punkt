b2ae861c079c4e653a7eb500e1b0cd2d72ec925c
Vladimir Mishel <vmishel@outlook.com>
2023-08-20 16:44:04 -0700

    Removed std::endl; from token_printer error message

--- applications/token_printer.cc
+++ applications/token_printer.cc
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
-		std::cerr << "Token printer application requires an input file argument" << std::endl;
+		std::cerr << "Token printer application requires an input file argument\n";
 		return 0;
 	}
 }
