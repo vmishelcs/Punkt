425e71fdb249dce92a13c7a5b52abfed0d02768d
Vladimir Mishel <vmishel@outlook.com>
2024-05-18 16:13:29 -0700

    Updated `CodegenContext` constructor

--- code_generator/codegen_context.cc
+++ code_generator/codegen_context.cc
@@ -3,6 +3,7 @@
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
+#include <llvm/MC/TargetRegistry.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Target/TargetMachine.h>
 #include <llvm/TargetParser/Host.h>
@@ -33,8 +34,31 @@ CodegenContext::CodegenContext(std::string module_id)
     : llvm_context(std::make_unique<llvm::LLVMContext>()),
       module(std::make_unique<llvm::Module>(module_id, *llvm_context)),
       builder(std::make_unique<llvm::IRBuilder<> >(*llvm_context)) {
-  // TODO: Find a way to avoid hardcoding data layout.
-  module->setDataLayout(
-      "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128");
-  module->setTargetTriple(llvm::sys::getDefaultTargetTriple());
+  // Initialize all targets for emitting object code.
+  llvm::InitializeAllTargetInfos();
+  llvm::InitializeAllTargets();
+  llvm::InitializeAllTargetMCs();
+  llvm::InitializeAllAsmParsers();
+  llvm::InitializeAllAsmPrinters();
+
+  auto target_triple = llvm::sys::getDefaultTargetTriple();
+
+  // Initialize a target and check for errors.
+  std::string error;
+  const llvm::Target *target =
+      llvm::TargetRegistry::lookupTarget(target_triple, error);
+  if (!target) {
+    PunktLogger::LogFatalInternalError(error);
+  }
+
+  // Create a generic CPU without any additional feature or target options.
+  auto cpu = "generic";
+  auto features = "";
+  llvm::TargetOptions opt;
+  llvm::TargetMachine *target_machine = target->createTargetMachine(
+      target_triple, cpu, features, opt, llvm::Reloc::PIC_);
+
+  // Configure the Module.
+  module->setDataLayout(target_machine->createDataLayout());
+  module->setTargetTriple(target_triple);
 }
