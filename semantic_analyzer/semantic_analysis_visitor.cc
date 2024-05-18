1548a274e6f497d732897958ef4d8f6279c90eb9
Vladimir Mishel <vmishel@outlook.com>
2024-05-18 15:14:39 -0700

    Removed comment

--- semantic_analyzer/semantic_analysis_visitor.cc
+++ semantic_analyzer/semantic_analysis_visitor.cc
@@ -81,31 +81,6 @@ void SemanticAnalysisVisitor::VisitLeave(DeallocStatementNode &node) {
   }
 }
 
-// void SemanticAnalysisVisitor::VisitEnter(DeclarationStatementNode &node) {
-//   bool is_mutable =
-//       KeywordToken::IsTokenKeyword(node.GetToken(), {Keyword::VAR});
-
-//   // Perform declaration here if the initializer is a lambda literal.
-//   ParseNode *initializer = node.GetInitializer();
-//   auto lambda_node = dynamic_cast<LambdaNode *>(initializer);
-//   if (!lambda_node) {
-//     return;
-//   }
-
-//   IdentifierNode *identifier = node.GetIdentifierNode();
-//   if (!identifier) {
-//     PunktLogger::LogFatalInternalError(
-//         "VisitEnter(DeclarationStatementNode&): "
-//         "incorrectly constructed declaration node.");
-//   }
-
-//   auto lambda_type = static_cast<LambdaType *>(lambda_node->GetType());
-
-//   identifier->SetType(lambda_type->CreateEquivalentType());
-//   DeclareInLocalScope(*identifier, is_mutable, identifier->GetType(),
-//                       SymbolType::FUNCTION);
-// }
-
 void SemanticAnalysisVisitor::VisitLeave(DeclarationStatementNode &node) {
   bool is_mutable =
       KeywordToken::IsTokenKeyword(node.GetToken(), {Keyword::VAR});
