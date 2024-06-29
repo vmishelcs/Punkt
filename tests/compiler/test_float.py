4d181e954b8a99bf1cb597745ba1a0904add3605
Vladimir Mishel <vmishel@outlook.com>
2024-06-28 23:32:50 -0700

    Added more tests

--- tests/compiler/test_float.py
+++ tests/compiler/test_float.py
@@ -96,3 +96,262 @@ class TestFloat(object):
       self._compile_helper.verify_actual_output(actual_output_path)
     except RuntimeError as ex:
       assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_14(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_15(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_16(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_17(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_18(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_19(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_20(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_21(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_22(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_23(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_24(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_25(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_26(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_27(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_28(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_29(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_30(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_31(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_32(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_33(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_34(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_35(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_36(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_37(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_38(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_39(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_40(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_41(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_42(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_43(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_44(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_45(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_46(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_47(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_48(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_49(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
+
+  def test_good_float_50(self):
+    try:
+      actual_output_path = self._compile_helper.compile_with_success()
+      self._compile_helper.verify_actual_output(actual_output_path)
+    except RuntimeError as ex:
+      assert False, "Exception was raised: " + repr(ex)
